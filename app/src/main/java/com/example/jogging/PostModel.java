679d4c4deb561c237b16ddc416e16a47b81fad7b
Hank <jeffjckiew@gmail.com>
2020-11-09 15:08:34 +0800

    V1

---
+++ app/src/main/java/com/example/jogging/PostModel.java
@@ -0,0 +1,59 @@
+package com.example.jogging;
+
+public class PostModel {
+    private int id;
+    private String breakfast;
+    private String lunch;
+    private String dinner;
+    private String extra;
+
+    public PostModel(){
+
+    }
+    public PostModel(String breakfast, String lunch, String dinner, String extra){
+        this.breakfast = breakfast;
+        this.lunch = lunch;
+        this.dinner = dinner;
+        this.extra = extra;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBreakfast() {
+        return breakfast;
+    }
+
+    public void setBreakfast(String breakfast) {
+        this.breakfast = breakfast;
+    }
+
+    public String getLunch() {
+        return lunch;
+    }
+
+    public void setLunch(String lunch) {
+        this.lunch = lunch;
+    }
+
+    public String getDinner() {
+        return dinner;
+    }
+
+    public void setDinner(String dinner) {
+        this.dinner = dinner;
+    }
+
+    public String getExtra() {
+        return extra;
+    }
+
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+}
